65ea35ccbae026f28839b1ef7872459611982b7b
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2022-07-13 10:27:54 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,17 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
-
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+    START_TEXT = """ """
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
+    UPGRADE_TEXT = " "
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = " "
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
-    CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
-    
-1. Send me any Telegram File.
-2. Choose appropriate option."""
+    CUSTOM_CAPTION_UL_FILE = """🔸 @OngoingAnimeNet
+
+📌 Powered By:- @Otaku_Network"""
+    HELP_USER = """ """
 
